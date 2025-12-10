7703f18bb8d15938182c80fd5e978d2607cf65e2
santhoshvijayakumarvdl-hash <santhoshvijayakumarvdl@gmail.com>
2025-12-10 17:53:16 +0530

    Create dist operation.sv

---
+++ DAY 3/dist operation.sv
@@ -0,0 +1,21 @@
+class constrain;
+  rand bit[3:0] c1;
+  rand bit a;
+  constraint c2{c1 dist{1:/6,2:/4};}
+  function void display();
+    $display("%d ", this.c1);
+  endfunction
+endclass
+
+module tb;
+  initial begin
+  constrain h;
+  h=new();
+    repeat(10)begin
+    h.randomize();
+    h.display();
+    end
+  end
+endmodule
+  
+  
